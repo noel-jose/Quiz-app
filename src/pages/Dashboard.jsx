f83eb285c045de2d8326c2b56ba62af3109611a7
Noel Jose <noeljose@SSLAP0013.local>
2023-03-10 22:51:31 +0530

    Created Dashboard page

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,26 @@
 import React from "react";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className="container mx-auto">
+      <div>
+        <h1 className="text-3xl font-semibold my-12">
+          View all Quizzes - Admin
+        </h1>
+      </div>
+      <main className="border rounded-md py-5 px-10">
+        <div className="flex justify-between my-12">
+          <h2 className="text-4xl">All Quizzes</h2>
+          <button className="bg-blue-400 px-4 py-2 text-white rounded-md">
+            New Quiz
+          </button>
+        </div>
+        <div className="flex flex-col">
+          
+        </div>
+      </main>
+    </div>
+  );
 };
 
 export default Dashboard;
