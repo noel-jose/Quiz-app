18d4ec4737a88ee8014bd5e1c5c862406b3bf2da
noel-jose <noeljosetp@gmail.com>
2023-03-20 19:22:03 +0530

    Removed unwanted comments from Attempt Quiz page

--- src/pages/AttemptQuiz.jsx
+++ src/pages/AttemptQuiz.jsx
@@ -9,7 +9,6 @@ import { useSelector } from "react-redux";
 
 const AttemptQuiz = () => {
   const { id } = useParams();
-  console.log(id);
   const answers = useSelector((state) => state.attempt);
 
   const [quiz, setQuiz] = useState([]);
@@ -19,29 +18,22 @@ const AttemptQuiz = () => {
   const [score, setScore] = useState(0);
 
   const fetchAQuiz = (id) => {
-    console.log(`${process.env.REACT_APP_BASE_URL}/${id}`);
     if (id != 0)
       axios.get(`${process.env.REACT_APP_BASE_URL}/${id}`).then((response) => {
         setQuiz(response.data);
-        console.log(response.data);
         setIsLoaded(true);
       });
   };
 
   const calculateScore = (answers, correctAnswers) => {
-    console.log("Answer", answers);
-    console.log("Question", correctAnswers);
-
     let s = 0;
     answers.map((answer) => {
       const answerId = answer.id;
       const index = correctAnswers.findIndex((item) => item.id == answerId);
-      console.log(index, correctAnswers[index].correctAnswers);
       const cAns = correctAnswers[index].correctAnswers;
       if (cAns.every((r) => answer.selected.includes(r))) s = s + 1;
     });
     setScore(s);
-    console.log("Score", score);
   };
 
   useEffect(() => {
